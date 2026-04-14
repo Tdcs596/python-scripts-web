9fd5b14d30d6a1a8b6fb8a69a6b849ec8c108c6e
Tdcs596 <tdcstechnologiespvtltd@gmail.com>
2026-04-15 00:30:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from script1 import script1_bp # Import kiya
-# from script2 import script2_bp (Aise hi baki bhi)
+from script2 import script2_bp
 
 app = Flask(__name__)
 
@@ -10,11 +10,15 @@ def home():
     return """
     <h1>🔥 Master Dashboard</h1>
     <a href="/script1"><button>Open Analyzer</button></a><br><br>
-    <a href="/script2"><button>Open Scraper</button></a>
+    <a href="/script2"><button>Open Scraper</button></a><br><br>
+    <a href="/script3"><button>Open EXE Converter</button></a>
     """
 
 # Yahan Script ko "Register" kiya
 app.register_blueprint(script1_bp, url_prefix='/script1')
+app.register_blueprint(script2_bp, url_prefix='/script2')
+app.register_blueprint(script3_bp, url_prefix='/script3')
+
 
 if __name__ == "__main__":
     import os
